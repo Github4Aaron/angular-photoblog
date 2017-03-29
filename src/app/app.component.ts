d48cf16890177b9654ca56ae6fd9a6efa51eaab6
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-29 19:56:41 -0400

    example of one-way data binding - template to class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
    emoji: string[] = ['🎉', '😍', '😜', '👍']; //If you delete this declaration, an error of "Property emoji does not exist..."
-    //TS can infer type defintions, when a property is assigned a value, no need to specify that it is an array of strings
+   activeEmoji: string;  //declaring the type as a string;    
+        //if you run string = 3; an error message appears                                   
+    changeEmoji() {
+        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
+        //most common method of random integer generator
     }
+ }
 
 
 //Component has two required properties: selector and template
 //Once the array has been set once, you can do away with the constructor.
+//TS can infer type defintions, when a property is assigned a value, no need to specify that it is an array of strings
